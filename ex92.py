200815fa372b14a6d91743383f1a4473897f0771
Kiki Lin <mikey2862@gmail.com>
2019-10-15 09:20:52 +0800

    add a new instance

--- ex92.py
+++ ex92.py
@@ -5,5 +5,9 @@ and call describe_restaurant() for each instance.
 """
 from ex91 import Restaurant
 
-cafe = Restaurant('mini', 'bakery', "10 o'clock")
+cafe = Restaurant('mini store', 'bread', "10 o'clock")
 cafe.describe_restaurant()
+
+store = Restaurant('disney store', 'mickey', "10 o'clock")
+store.describe_restaurant()
+store.open_restaurant()

96de1b94fb1c087a2e9d0d63d02062bcd452a786
Kiki Lin <mikey2862@gmail.com>
2019-10-15 09:05:28 +0800

    upload the second exercise

---
+++ ex92.py
@@ -0,0 +1,9 @@
+"""
+Start with your class from Exercise 9-1.
+Create three different instances from the class,
+and call describe_restaurant() for each instance.
+"""
+from ex91 import Restaurant
+
+cafe = Restaurant('mini', 'bakery', "10 o'clock")
+cafe.describe_restaurant()
